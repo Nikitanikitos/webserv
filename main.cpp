363eed2cf1f45d4902da684903b229fcb08a9d27
Irving Micah <imicah@va-i1.kzn.21-school.ru>
2020-11-24 06:30:06 +0300

    qwe

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nikita <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 18:05:50 by imicah            #+#    #+#             */
-/*   Updated: 2020/11/22 13:36:50 by nikita           ###   ########.fr       */
+/*   Updated: 2020/11/24 03:07:14 by imicah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,58 +16,75 @@
 #include <fcntl.h>
 #include <cstring>
 #include <arpa/inet.h>
+#include <signal.h>
 #include "libft.hpp"
 
-int main() {
+int main(int ac, char **av, char **env) {
 	fd_set				sockets_set;
-	fd_set				accepts_set;
-	int					fd_socket;
+	int					fd_socket_80 = 1;
+	int					fd_socket_8080;
 	int					fd_accept;
 	struct sockaddr_in	sock_addr;
 	char				buf[128];
 	int					bytes;
 	std::string			request;
-	struct in_addr		ip;
 
-	ft_memset(buf, 0, 512);
-	ft_memset(&sock_addr, 0, sizeof(sock_addr));
-	sock_addr.sin_family = PF_INET;
-	sock_addr.sin_port = ft_htons(80);
-	sock_addr.sin_addr.s_addr = ft_htonl(INADDR_ANY);
-
-	ip.s_addr = INADDR_ANY;
-	char *q = inet_ntoa(ip);
-
-	fd_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	int one = 1;
-	setsockopt(fd_socket, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
-	bind(fd_socket, (struct sockaddr *) &sock_addr, sizeof(sock_addr));
-
-	fcntl(fd_socket, F_SETFL, O_NONBLOCK);
+//	ft_memset(buf, 0, 512);
 	FD_ZERO(&sockets_set);
-	FD_ZERO(&accepts_set);
-	FD_SET(fd_socket, &sockets_set);
+//
+//	ft_memset(&sock_addr, 0, sizeof(sock_addr));
+//	sock_addr.sin_family = PF_INET;
+//	sock_addr.sin_port = ft_htons(80);
+//	sock_addr.sin_addr.s_addr = ft_htonl(INADDR_ANY);
+//
+//	fd_socket_80 = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+//	bind(fd_socket_80, (struct sockaddr *) &sock_addr, sizeof(sock_addr));
+//	fcntl(fd_socket_80, F_SETFL, O_NONBLOCK);
+//
+//	ft_memset(&sock_addr, 0, sizeof(sock_addr));
+//	sock_addr.sin_family = PF_INET;
+//	sock_addr.sin_port = ft_htons(8080);
+//	sock_addr.sin_addr.s_addr = ft_htonl(INADDR_ANY);
+//
+//	fd_socket_8080 = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+//	bind(fd_socket_8080, (struct sockaddr *) &sock_addr, sizeof(sock_addr));
+//	fcntl(fd_socket_8080, F_SETFL, O_NONBLOCK);
+//
+	FD_SET(fd_socket_80, &sockets_set);
 
-	listen(fd_socket, 10);
+	while (1) {
+		select(fd_socket_80 + 1, nullptr, &sockets_set, nullptr, nullptr);
 
+		write(fd_socket_80, "wqe", 3);
+	}
+
+	//	FD_SET(fd_socket_8080, &sockets_set);
+//
+//	listen(fd_socket_80, 10);
+//	listen(fd_socket_8080, 10);
+
+	int 	pid = getpid();
+	std::cout << "eqw" << std::endl;
+	kill(pid, SIGSTOP);
 	while (true) {
-		select(fd_socket + 1, &sockets_set, nullptr, nullptr, nullptr);
+		select(fd_socket_8080 + 1, &sockets_set, nullptr, nullptr, nullptr);
+		std::cout << "eqw" << std::endl;
 
-		fd_accept = accept(fd_socket, nullptr, nullptr);
+		fd_accept = accept(fd_socket_80, nullptr, nullptr);
 		if (fd_accept > 0) {
 			while (true) {
 				ft_memset(buf, 0, 128);
 				bytes = recv(fd_accept, buf, 128, 0);
-				if (strncmp(buf, "\r\n", 2) == 0)
+				if (strncmp(buf, "\r\n", 2) == 0 || bytes == 0)
 					break;
+				else if (!strcmp(buf, "exit\r\n"))
+					exit(0);
 
-				request += buf;
+				send(fd_accept, buf, bytes, 0);
 			}
-			send(fd_accept, request.c_str(), request.size(), 0);
 			close(fd_accept);
-			break;
+//			break;
 		}
 	}
-//	exit(0);
 	return (0);
 }
