c66a34620d3d429dfd384be62e4297917af94e6a
Irving Micah <imicah@va-i1.kzn.21-school.ru>
2020-11-24 06:29:54 +0300

    added workers

--- WebServ/WebServ.cpp
+++ WebServ/WebServ.cpp
@@ -3,45 +3,131 @@
 /*                                                        :::      ::::::::   */
 /*   WebServ.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nikita <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 19:48:56 by nikita            #+#    #+#             */
-/*   Updated: 2020/11/22 14:20:45 by nikita           ###   ########.fr       */
+/*   Updated: 2020/11/24 05:41:03 by imicah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "WebServ.hpp"
 
-WebServ::WebServ(const std::vector<VirtualServer>& list_virtual_servers) : _list_virtual_servers(list_virtual_servers) { }
+WebServ::WebServ(const std::vector<VirtualServer>& list_virtual_servers) : _list_virtual_servers(list_virtual_servers) {
+	FD_ZERO(&_set_of_vs_sockets);
 
-WebServ::~WebServ() {
-
+	for (const VirtualServer& server : _list_virtual_servers)
+		for (const int fd_socket : server.vs_sockets) {
+			_vs_sockets.push_back(fd_socket);
+			FD_SET(fd_socket, &_set_of_vs_sockets);
+		}
+	_create_workers();
 }
 
-std::map<std::string, std::string> WebServ::_check_request_header() {
+WebServ::~WebServ() {
+	std::pair<int, int>		worker;
+
+	while (!_worker_queue.empty()){
+		worker = _pop_worker();
+		close(worker.first);
+	}
+}
+
+std::map<std::string, std::string>		WebServ::_check_request_header() {
 	return (std::map<std::string, std::string>());
 }
 
-void WebServ::_execute_cgi_client() {
+void		WebServ::_execute_cgi_client() {
 
 }
 
-void WebServ::_parse_request_body() {
+void		WebServ::_parse_request_body() {
 
 }
 
-void WebServ::_get_request(VirtualServer&) {
+void		WebServ::_give_response() {
 
 }
 
-void WebServ::_give_response() {
-
+void		WebServ::_serve_client(int client_socket) {
+	VirtualServer	virtual_server = _get_request(client_socket);
 }
 
-void WebServ::run_server() {
+void		WebServ::_create_workers() {
+	int 	pipe_fd[2];
 
+	FD_ZERO(&_set_of_pipes);
+	for (int i = 0; i < _number_workers; ++i) {
+		pipe(pipe_fd);
+		if (fork() == 0) {
+			close(pipe_fd[STDOUT_FILENO]);
+			_worker(pipe_fd[STDIN_FILENO]);
+		}
+		else {
+			close(pipe_fd[STDIN_FILENO]);
+			FD_SET(pipe_fd[STDOUT_FILENO], &_set_of_pipes);
+			_worker_queue.push(std::make_pair(pipe_fd[STDOUT_FILENO], 0));
+		}
+	}
 }
 
-void WebServ::serve_client(int client_socket) {
+void		WebServ::_worker(int pipe_fd) {
+	fd_set	pipe_set;
+	int		file_position;
+	int 	client_socket;
 
+	file_position = 0;
+	FD_ZERO(&pipe_set);
+	FD_SET(pipe_fd, &pipe_set);
+	while (true) {
+		select(pipe_fd + 1, &pipe_set, nullptr, nullptr, nullptr);
+		read(pipe_fd, (char*)&client_socket, 4);
+		file_position += 4;
+		if (file_position >= PIPE_BUFFER_SIZE)
+			_pointer_file_to_start(pipe_fd, file_position);
+		_serve_client(client_socket);
+	}
+}
+
+void		WebServ::run_server() {
+	int					 	client_socket;
+	std::pair<int, int>		worker;
+
+	while (true) {
+		select(_vs_sockets.back() + 1, &_set_of_vs_sockets, nullptr, nullptr, nullptr);
+		_get_accept_from_ready_sockets();
+	}
+}
+
+void		WebServ::_pointer_file_to_start(int& fd, int& file_position) {
+	file_position = 0;
+	lseek(fd, 0, SEEK_SET);
+}
+
+VirtualServer			WebServ::_get_request(int client_socket) {
+	return (VirtualServer());
+}
+
+void		WebServ::_get_accept_from_ready_sockets() {
+	int					 	client_socket;
+	std::pair<int, int>		worker;
+
+	for (int socket : _vs_sockets) {
+		if (FD_ISSET(socket, &_set_of_vs_sockets)) {
+			while ((client_socket = accept(socket, nullptr, nullptr)) != -1) {
+				worker = _pop_worker();
+				write(worker.first, (char*)&client_socket, 4);
+				worker.second += 4;
+				if (worker.second >= PIPE_BUFFER_SIZE)
+					_pointer_file_to_start(worker.first, worker.second);
+				_worker_queue.push(worker);
+			}
+		}
+	}
+}
+
+std::pair<int, int>		WebServ::_pop_worker() {
+	std::pair<int, int>		worker = _worker_queue.front();
+
+	_worker_queue.pop();
+	return (worker);
 }
--- WebServ/WebServ.hpp
+++ WebServ/WebServ.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   WebServ.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nikita <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 19:49:07 by nikita            #+#    #+#             */
-/*   Updated: 2020/11/22 14:20:45 by nikita           ###   ########.fr       */
+/*   Updated: 2020/11/24 05:41:03 by imicah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,50 @@
 
 # include <string>
 # include <map>
+# include <queue>
 # include <zconf.h>
 # include "VirtualServer.hpp"
 
+# define PIPE_BUFFER_SIZE	64000
+
 class WebServ {
 private:
-	fd_set							_set_vs_sockets;
-	std::vector<VirtualServer>		_list_virtual_servers;
+	fd_set									_set_of_vs_sockets;
+	fd_set									_set_of_pipes;
+	std::vector<int>						_vs_sockets;
+	std::vector<VirtualServer>				_list_virtual_servers;
+	std::queue<std::pair<int, int> >		_worker_queue;
+	int 									_number_workers;
 
 	std::map<std::string, std::string>	_check_request_header();
 	/* Метод парсит заголовок из request, сохраняет их в словарь и возвращает */
 
-	void	_execute_cgi_client();
+	void _get_accept_from_ready_sockets();
 
-	void	_parse_request_body(); // TODO что метод возращает и возвращает ли вообще?
+	void				_execute_cgi_client();
+
+	void				_parse_request_body(); // TODO что метод возращает и возвращает ли вообще?
 	/* Метод парсит тело запроса */
 
-	void	_get_request(VirtualServer&);
+	void					_create_workers();
+	[[noreturn]] void		_worker(int);
+	std::pair<int, int>		_pop_worker();
+	void					_pointer_file_to_start(int&, int&);
+
+	VirtualServer			_get_request(int);
 	/* Метод получает данные запроса через функцию recv().
 	 * Из первой строки получает метод, путь и заносит в объект Request.
 	 * Далее парсит заголовки, заносит их в словарь и проверяет корректность заголовка через _check_request_header().
 	 * Далее парсит тело запроса через _parse_request_body() (если тело запроса есть).
 	 * Возвращает объект Request() */
 
-	void	_give_response();
+	void					_give_response();
 	/* Принимает объект Request(), создает объект Response(), составляет тело ответа и отсылает его через send() */
+
+	void				_serve_client(int);
+	/* Метод вызывает функцию accept() на сокет и вызывает метод _get_request(),
+	 * который возвращает объект Request.
+	 * вызов метода _give_response() */
 
 public:
 	explicit WebServ(const std::vector<VirtualServer>&);
@@ -47,14 +66,9 @@ public:
 
 	virtual ~WebServ();
 
-	void	run_server();
+	[[noreturn]] void	run_server();
 	/* Метод запускает сервер. Вызывает select() на серверные сокеты с бесконечным тайм-аутом,
 	 * Проверяет в какой сокет поступило соединение и вызывает метод serve_client(), передавая туда сокет клиента */
-
-	void	serve_client(int);
-	/* Метод вызывает функцию accept() на сокет и вызывает метод _get_request(),
-	 * который возвращает объект Request.
-	 * вызов метода _give_response() */
 };
 
 #endif //WEBSERV_WEBSERV_HPP
