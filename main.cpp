b79ca052e0fe1ecc43d19883d921b29e66dcd178
Irving Micah <imicah@va-i1.kzn.21-school.ru>
2020-11-30 18:02:16 +0300

    unimportant changes

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 18:05:50 by imicah            #+#    #+#             */
-/*   Updated: 2020/11/24 20:30:19 by imicah           ###   ########.fr       */
+/*   Updated: 2020/11/29 15:47:10 by imicah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,83 +20,84 @@
 #include <sys/time.h>
 #include "libft.hpp"
 
-//int main(int ac, char **av, char **env) {
-//	fd_set				sockets_set;
-//	int					fd_socket_80 = 1;
-//	int					fd_socket_8080;
-//	int					fd_accept;
-//	struct sockaddr_in	sock_addr;
-//	char				buf[128];
-//	int					bytes;
-//	std::string			request;
+int main(int ac, char **av, char **env) {
+	fd_set				sockets_set;
+	int					fd_socket_80 = 1;
+	int					fd_socket_8080;
+	int					fd_accept;
+	struct sockaddr_in	sock_addr;
+	char				buf[128];
+	int					bytes;
+	std::string			request;
+
+	ft_memset(buf, 0, 512);
+	FD_ZERO(&sockets_set);
+
+	ft_memset(&sock_addr, 0, sizeof(sock_addr));
+	sock_addr.sin_family = PF_INET;
+	sock_addr.sin_port = ft_htons(80);
+	sock_addr.sin_addr.s_addr = ft_htonl(INADDR_ANY);
+
+	fd_socket_80 = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	bind(fd_socket_80, (struct sockaddr *) &sock_addr, sizeof(sock_addr));
+	fcntl(fd_socket_80, F_SETFL, O_NONBLOCK);
+
+	ft_memset(&sock_addr, 0, sizeof(sock_addr));
+	sock_addr.sin_family = PF_INET;
+	sock_addr.sin_port = ft_htons(8080);
+	sock_addr.sin_addr.s_addr = ft_htonl(INADDR_ANY);
+
+	fd_socket_8080 = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	bind(fd_socket_8080, (struct sockaddr *) &sock_addr, sizeof(sock_addr));
+	fcntl(fd_socket_8080, F_SETFL, O_NONBLOCK);
+
+	FD_SET(fd_socket_80, &sockets_set);
+
+//	while (1) {
+//		select(fd_socket_80 + 1, nullptr, &sockets_set, nullptr, nullptr);
 //
-//	ft_memset(buf, 0, 512);
-//	FD_ZERO(&sockets_set);
-//
-//	ft_memset(&sock_addr, 0, sizeof(sock_addr));
-//	sock_addr.sin_family = PF_INET;
-//	sock_addr.sin_port = ft_htons(80);
-//	sock_addr.sin_addr.s_addr = ft_htonl(INADDR_ANY);
-//
-//	fd_socket_80 = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-//	bind(fd_socket_80, (struct sockaddr *) &sock_addr, sizeof(sock_addr));
-//	fcntl(fd_socket_80, F_SETFL, O_NONBLOCK);
-//
-//	ft_memset(&sock_addr, 0, sizeof(sock_addr));
-//	sock_addr.sin_family = PF_INET;
-//	sock_addr.sin_port = ft_htons(8080);
-//	sock_addr.sin_addr.s_addr = ft_htonl(INADDR_ANY);
-//
-//	fd_socket_8080 = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-//	bind(fd_socket_8080, (struct sockaddr *) &sock_addr, sizeof(sock_addr));
-//	fcntl(fd_socket_8080, F_SETFL, O_NONBLOCK);
-//
-//	FD_SET(fd_socket_80, &sockets_set);
-//
-////	while (1) {
-////		select(fd_socket_80 + 1, nullptr, &sockets_set, nullptr, nullptr);
-////
-////		write(fd_socket_80, "wqe", 3);
-////	}
-//
-//	FD_SET(fd_socket_8080, &sockets_set);
-//
-//	listen(fd_socket_80, 10);
-//	listen(fd_socket_8080, 10);
-//
-//	while (true) {
-//		select(fd_socket_8080 + 1, &sockets_set, nullptr, nullptr, nullptr);
-//		std::cout << "eqw" << std::endl;
-//
-//		fd_accept = accept(fd_socket_80, nullptr, nullptr);
-//		if (fd_accept > 0) {
-//			while (true) {
-//				ft_memset(buf, 0, 128);
-//				bytes = recv(fd_accept, buf, 128, 0);
-//				if (strncmp(buf, "\r\n", 2) == 0 || bytes == 0)
-//					break;
-//				else if (!strcmp(buf, "exit\r\n"))
-//					exit(0);
-//
-//				send(fd_accept, buf, bytes, 0);
-//			}
-//			close(fd_accept);
-////			break;
-//		}
+//		write(fd_socket_80, "wqe", 3);
 //	}
-//	return (0);
-//}
 
-int 	main () {
-	struct tm*		timeinfo;
-	struct tm		ft_timeinfo;
-	struct timeval	tv;
-	char			buffer[80];
+	FD_SET(fd_socket_8080, &sockets_set);
 
-	gettimeofday(&tv, nullptr);
+	listen(fd_socket_80, 10);
+	listen(fd_socket_8080, 10);
 
-	ft_localtime(ft_timeinfo, tv.tv_sec);
-	timeinfo = localtime(&tv.tv_sec);
+	while (true) {
+		select(fd_socket_8080 + 1, &sockets_set, nullptr, nullptr, nullptr);
+		std::cout << "eqw" << std::endl;
 
-	strptime (buffer, "Now it's %I:%M%p.", timeinfo);
+		fd_accept = accept(fd_socket_80, nullptr, nullptr);
+		if (fd_accept > 0) {
+			while (true) {
+				ft_memset(buf, 0, 128);
+				bytes = recv(fd_accept, buf, 128, MSG_PEEK);
+				if (strncmp(buf, "\r\n", 2) == 0 || bytes == 0)
+					break;
+				else if (!strcmp(buf, "exit\r\n"))
+					exit(0);
+
+				write(fd_accept, "hhelo", 5);
+				send(fd_accept, buf, bytes, 0);
+			}
+			close(fd_accept);
+//			break;
+		}
+	}
+	return (0);
 }
+
+//int 	main () {
+//	struct tm*		timeinfo;
+//	struct tm		ft_timeinfo;
+//	struct timeval	tv;
+//	char			buffer[80];
+//
+//	gettimeofday(&tv, nullptr);
+//
+//	ft_localtime(ft_timeinfo, tv.tv_sec);
+//	timeinfo = localtime(&tv.tv_sec);
+//
+//	strptime (buffer, "Now it's %I:%M%p.", timeinfo);
+//}
