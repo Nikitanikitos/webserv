453dea968df51fc9c6b2b537ed2318ae6c66682c
Irving Micah <imicah@va-i3.kzn.21-school.ru>
2020-12-05 20:23:46 +0300

    added ThreadPool class

--- ThreadPool/ThreadPool.cpp
+++ ThreadPool/ThreadPool.cpp
@@ -1,5 +1,38 @@
-//
-// Created by Irving Micah on 12/5/20.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ThreadPool.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/05 18:37:33 by imicah            #+#    #+#             */
+/*   Updated: 2020/12/05 18:37:33 by imicah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "ThreadPool.hpp"
+
+ThreadPool::ThreadPool() {
+	if ((_mutex_check_tasks_queue = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t))) == nullptr)
+		throw std::exception();
+	pthread_mutex_init(_mutex_check_tasks_queue, nullptr); }
+
+ThreadPool::~ThreadPool() {
+	pthread_mutex_destroy(_mutex_check_tasks_queue);
+	free(_mutex_check_tasks_queue);
+}
+
+HttpObject& ThreadPool::pop_task() {
+	HttpObject&		http_object = _tasks_queue.front();
+
+	_tasks_queue.pop();
+	return (http_object);
+}
+
+pthread_mutex_t*	ThreadPool::get_mutex_check_tasks_queue() const { return (_mutex_check_tasks_queue); }
+
+void ThreadPool::push_task(const HttpObject& http_object) { _tasks_queue.push(http_object); }
+
+bool ThreadPool::queue_is_empty() const {
+	return (_tasks_queue.empty());
+}
--- ThreadPool/ThreadPool.hpp
+++ ThreadPool/ThreadPool.hpp
@@ -1,15 +1,35 @@
-//
-// Created by Irving Micah on 12/5/20.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ThreadPool.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/05 18:34:43 by imicah            #+#    #+#             */
+/*   Updated: 2020/12/05 18:34:43 by imicah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef WEBSERV_THREADPOOL_HPP
-#define WEBSERV_THREADPOOL_HPP
+# define WEBSERV_THREADPOOL_HPP
 
 # include <queue>
+# include "HttpObject.hpp"
 
 class ThreadPool {
 private:
 	std::queue<HttpObject>		_tasks_queue;
+	pthread_mutex_t*			_mutex_check_tasks_queue;
+
+public:
+	ThreadPool();
+	~ThreadPool();
+
+	[[nodiscard]] pthread_mutex_t*		get_mutex_check_tasks_queue() const;
+	[[nodiscard]] bool					queue_is_empty() const;
+
+	HttpObject&							pop_task();
+	void 								push_task(const HttpObject&);
 
 };
 
