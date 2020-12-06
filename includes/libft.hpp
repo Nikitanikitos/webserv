864a649063665740f2858c25a7be9e0ad9b824e5
Nikita <nikitos_athletos@mail.ru>
2020-12-06 14:04:02 +0300

    added ft_getline func

--- includes/libft.hpp
+++ includes/libft.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: nikita <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 12:50:34 by imicah            #+#    #+#             */
-/*   Updated: 2020/12/05 14:43:17 by imicah           ###   ########.fr       */
+/*   Updated: 2020/12/06 02:22:00 by nikita           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,26 @@
 # include <dirent.h>
 # include <sys/stat.h>
 # include <thread>
+# include <fcntl.h>
+# include <zconf.h>
 
 # define HTTP_VERSION	(std::string)"HTTP/1.1"
 # define SERVER_VERSION	"WebServ/0.1"
 # define CRLF			"\r\n"
 # define SP				" "
 
-void*		ft_memset(void*, int, size_t);
+void*					ft_memset(void*, int, size_t);
 
-uint16_t	ft_htons(uint16_t);
-uint32_t	ft_htonl(uint32_t);
+uint16_t				ft_htons(uint16_t);
+uint32_t				ft_htonl(uint32_t);
 
-int			ft_atoi(const char* nptr);
+int						ft_atoi(const char* nptr);
 
-void		ft_localtime(tm&, time_t);
+void					ft_localtime(tm&, time_t);
+
+std::string				ft_getdate();
+std::string				ft_getfile(const char *path);
 
 [[noreturn]] void*		worker(void* arg);
-
-std::string		ft_getdate();
 
 #endif //WEBSERV_LIBFT_HPP
--- libft/ft_getfile.cpp
+++ libft/ft_getfile.cpp
@@ -1,4 +1,26 @@
-//
-// Created by nikita on 06.12.2020.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_getfile.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nikita <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/06 02:11:18 by nikita            #+#    #+#             */
+/*   Updated: 2020/12/06 02:22:00 by nikita           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+# include "libft.hpp"
+
+std::string		ft_getfile(const char *path) {
+	const int 			fd = open(path, O_RDONLY);
+	std::string			result;
+	char				buff[512];
+
+	while ((read(fd, &buff, 512)) > 0) {
+		result.append(buff);
+		ft_memset(&buff, 0, 512);
+	}
+	close(fd);
+	return (result);
+}
