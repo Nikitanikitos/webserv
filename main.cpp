61f709ad769f2a34a5cd5d9819197f2a470c4bd4
Irving Micah <imicah@ge-d4.kzn.21-school.ru>
2020-12-18 17:47:40 +0300

    qwe

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: imicah <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 18:05:50 by imicah            #+#    #+#             */
-/*   Updated: 2020/12/17 19:34:18 by imicah           ###   ########.fr       */
+/*   Updated: 2020/12/18 16:33:18 by imicah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,9 @@ char*	get_file_name(char *arg) {
 int		main(int ac, char **av, char **env) {
 	std::string					number_of_workers;
 	ParseConfigFile				parse(get_file_name(av[1]));
-	std::vector<VirtualServer>	list_virtual_server = parse.ParseFile(number_of_workers);
+	std::vector<VirtualServer*>	list_virtual_server = parse.ParseFile(number_of_workers);
 	WebServ						server(std::stoi(number_of_workers));
 
-	server.ReserveSize(list_virtual_server.size());
 	for (int i = 0; i < list_virtual_server.size(); ++i)
 		server.AddVirtualServer(list_virtual_server[i]);
 
