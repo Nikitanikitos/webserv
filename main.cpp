e2c62f2e8318bc30e83f93a851951fdee422b252
nikita <nikitos.athletos@gmail.com>
2021-01-21 01:16:22 +0300

    added try catch

--- main.cpp
+++ main.cpp
@@ -14,22 +14,27 @@
 #include "WebServ.hpp"
 #include "ParseConfigFile.hpp"
 #include <algorithm>
+#include <iostream>
 
 int		exit() {
 	WebServ::working = 0;
 	return (0);
 }
 
-int		main(int ac, char **av, char **env) {
+int		main(int ac, char **av) {
 	std::string					number_of_workers;
-	ParseConfigFile				parse(((ac == 2) ? av[1] : (char*)"default.conf"));
-	std::vector<VirtualServer*>	list_virtual_server = parse.ParseFile(number_of_workers);
-	WebServ						server(ft_atoi(number_of_workers.c_str()));
+	try {
+		ParseConfigFile				parse(((ac == 2) ? av[1] : (char*)"default.conf"));
+		std::vector<VirtualServer*>	list_virtual_server = parse.ParseFile(number_of_workers);
+		WebServ						server(ft_atoi(number_of_workers.c_str()));
 
-	for (int i = 0; i < list_virtual_server.size(); ++i)
-		server.addVirtualServer(list_virtual_server[i]);
+		for (int i = 0; i < list_virtual_server.size(); ++i)
+			server.addVirtualServer(list_virtual_server[i]);
 
-	signal(SIGINT, exit);
-	server.runServer();
+		signal(SIGINT, exit);
+		server.runServer();
+	}
+	catch (ParseConfigFile::ParseConfigFileException& e)
+		{ std::cout << e.what() << std::endl;}
 	return (0);
 }
